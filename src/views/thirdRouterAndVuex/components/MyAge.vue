847f80497931bcc7cc3c4967fb44f81a59144e3b
XDJcc <2045659302@qq.com>
2023-08-25 21:48:55 +0800

    测试推送

--- src/views/thirdRouterAndVuex/components/MyAge.vue
+++ src/views/thirdRouterAndVuex/components/MyAge.vue
@@ -5,9 +5,7 @@ const myAge = ref<string>("张三今天是18岁生日");
 </script>
 <template>
   <el-row>
-    <el-col :spam="6">
-      {{ myAge }}
-    </el-col>
+    <el-col :spam="6"> {{ myAge }}1111 </el-col>
   </el-row>
 </template>
 <style lang="scss" scoped></style>
